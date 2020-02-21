30136e10da06bc28d2b737053c34fb427053da7b
Leah Lundqvist <hampus0505@gmail.com>
2020-02-22 00:12:19 +0100

    Created classes

---
+++ Sources/DisQR/Crypto.swift
@@ -0,0 +1,58 @@
+//
+//  Crypto.swift
+//  
+//
+//  Created by Leah Lundqvist on 2020-02-21.
+//
+
+import Foundation
+import RSAPublicKeyExporter
+import CommonCrypto
+
+extension DisQR {
+    class Crypto {
+        var privateKey: SecKey
+        var publicKey: SecKey
+        public var publicBase64: String
+        
+        init () throws {
+            let attributes: [String: Any] = [
+                kSecAttrKeyType as String: kSecAttrKeyTypeRSA,
+                kSecAttrKeySizeInBits as String: 2048,
+                kSecPrivateKeyAttrs as String: []
+            ]
+            
+            var error: Unmanaged<CFError>?
+            guard let pK = SecKeyCreateRandomKey(attributes as CFDictionary, &error) else { throw error as! Error }
+            self.privateKey = pK
+            
+            self.publicKey = SecKeyCopyPublicKey(self.privateKey)!
+            guard let pkData = SecKeyCopyExternalRepresentation(publicKey, &error) else { throw error as! Error }
+            self.publicBase64 = DisQR.Crypto.base64Encode(RSAPublicKeyExporter().toSubjectPublicKeyInfo(pkData as Data))
+        }
+        
+        func decrypt(data: String) -> Data? {
+            let payload = DisQR.Crypto.base64Decode(data)
+            var error: Unmanaged<CFError>?
+            guard let decrypted = SecKeyCreateDecryptedData(privateKey, SecKeyAlgorithm.rsaEncryptionOAEPSHA256, payload as CFData, &error) else { return nil }
+            return decrypted as Data
+        }
+        
+        public static func sha256(data : Data) -> Data {
+            var hash = [UInt8](repeating: 0,  count: Int(CC_SHA256_DIGEST_LENGTH))
+            data.withUnsafeBytes {
+                _ = CC_SHA256($0.baseAddress, CC_LONG(data.count), &hash)
+            }
+            return Data(hash)
+        }
+        
+        public static func base64Encode(_ data: Data) -> String {
+            return data.base64EncodedString(options: [])
+        }
+        
+        public static func base64Decode(_ strBase64: String) -> Data {
+            let data = Data(base64Encoded: strBase64, options: [])
+            return data!
+        }
+    }
+}
---
+++ Sources/DisQR/Socket.swift
@@ -0,0 +1,75 @@
+//
+//  Socket.swift
+//  
+//
+//  Created by Leah Lundqvist on 2020-02-21.
+//
+
+import Foundation
+import Starscream
+import SwiftyJSON
+
+extension DisQR {
+    enum SocketState {
+        case disconnected
+        case connecting
+        case connected
+    }
+    
+    typealias SocketHandlerFunc = (JSON) -> Void
+    
+    class Socket: WebSocketDelegate {
+        var request:URLRequest = URLRequest(url: URL(string: "wss://remote-auth-gateway.discord.gg/?v=1")!)
+        var ws: WebSocket
+        
+        var state: SocketState = SocketState.disconnected
+        var handlers:[String:SocketHandlerFunc] = [:]
+        
+        init() {
+            self.ws = WebSocket(request: request)
+            self.ws.delegate = self
+        }
+        
+        func setHandlers(handlers:[String:SocketHandlerFunc]) {
+            self.handlers = handlers
+        }
+        
+        func connect() {
+            self.state = .connecting
+            self.ws.connect()
+        }
+        
+        func didReceive(event: WebSocketEvent, client: WebSocket) {
+            switch event {
+            case .connected(_):
+                self.state = .connected
+                if self.handlers["connected"] != nil { self.handlers["connected"]!(JSON()) }
+            case .disconnected(_, _):
+                self.state = .disconnected
+                if self.handlers["disconnected"] != nil { self.handlers["disconnected"]!(JSON()) }
+            case .text(let string):
+                do {
+                    let json = try JSON(data: string.data(using: .utf8, allowLossyConversion: false)!)
+                    print(json)
+                    if self.handlers[json["op"].stringValue] != nil { self.handlers[json["op"].stringValue]!(json) }
+                } catch {
+                    print(error)
+                }
+            case .binary(_):
+                break;
+            case .ping(_):
+                break
+            case .pong(_):
+                break
+            case .viablityChanged(_):
+                break
+            case .reconnectSuggested(_):
+                break
+            case .cancelled:
+                self.state = .disconnected
+            case .error:
+                self.state = .disconnected
+            }
+        }
+    }
+}
